d8fdba3ecfd7c23320da6336ce4e9a99175d5942
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2022-04-13 01:37:40 +0200

    Removed main and created demo

--- configure.sh
+++ configure.sh
@@ -15,18 +15,21 @@ export SOURCE_DIR="${PROJECT_DIR}"
 # Building
 cmake                                               \
 -B"${BUILD_DIR}/${BUILD_TYPE}" -H"${SOURCE_DIR}"    \
--DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+-DCMAKE_BUILD_TYPE=${BUILD_TYPE}                    \
+-DBIN_DIR="${BIN_DIR}"
 
 cmake --build "${BUILD_DIR}/${BUILD_TYPE}" -- -j$(nproc)
 
 # Creating links in bin
 rm -rf "${BIN_DIR}"
 mkdir -p "${BIN_DIR}"
-ln -s "${BUILD_DIR}/${BUILD_TYPE}/test/run_tests" "${BIN_DIR}/run_tests"
-ln -s "${BUILD_DIR}/${BUILD_TYPE}/src/pathtracer" "${BIN_DIR}/pathtracer"
 
-chmod +x "${BIN_DIR}/run_tests"
-chmod +x "${BIN_DIR}/pathtracer"
+ln -s "${BUILD_DIR}/${BUILD_TYPE}/test/run_tests" "${BIN_DIR}/run_tests"
+ln -s "${BUILD_DIR}/${BUILD_TYPE}/demos/three_balls" "${BIN_DIR}/demo_three_balls"
+
+for file in "${BIN_DIR}"; do
+    chmod +x "${file}"
+done
 
 # Compile commands
 ln -sf "${BUILD_DIR}/${BUILD_TYPE}/compile_commands.json" "${PROJECT_DIR}/compile_commands.json"
